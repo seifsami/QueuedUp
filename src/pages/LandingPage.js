c9b913b1c8811c62a9aa82ec8e092386b56b4eb0
Seleem <sseleem1601@gmail.com>
2023-12-07 17:33:10 -0500

    Placeholder landing page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,31 +1,30 @@
 import React from 'react';
-import { Box, Text, Button, Image, Flex, Spacer } from '@chakra-ui/react';
-import Header from '../components/Header'; // Assuming you have a Header component
+import { Box, Text, Button, Flex, Image, VStack } from '@chakra-ui/react';
 
 const LandingPage = () => {
     return (
         <>
-            <Flex direction="column" align="center" justify="center" minHeight="100vh" bg="white" px={4}>
-                <Box maxW={{ xl: "1200px" }} width="full">
-                    <Text fontSize="5xl" fontWeight="bold" textAlign="center" mb={6}>
+            <Flex
+                direction="column"
+                align="center"
+                justify="center"
+                minHeight="100vh"
+                bgImage={`${process.env.PUBLIC_URL}/landing.jpg`} // Background image
+                bgPosition="center"
+                bgRepeat="no-repeat"
+                bgSize="cover"
+            >
+                <VStack spacing={6} maxW={{ xl: "1200px" }} width="full" px={4} bg="rgba(255, 255, 255, 0.6)" borderRadius="lg" p={6}>
+                    <Text fontSize="5xl" fontWeight="bold" textAlign="center" color="black">
                         Welcome to Our World of Books!
                     </Text>
-                    <Text fontSize="xl" textAlign="center" mb={8}>
+                    <Text fontSize="xl" textAlign="center" color="black">
                         Discover your next great read with us. Explore new releases, trending titles, and timeless classics.
                     </Text>
-                    <Flex justify="center">
-                        <Button colorScheme="blue" mr={3}>
-                            Browse Books
-                        </Button>
-                        <Button colorScheme="green">
-                            Join our Community
-                        </Button>
-                    </Flex>
-                </Box>
-                <Spacer />
-                <Box maxW={{ xl: "1200px" }} width="full">
-                    <Image src={`${process.env.PUBLIC_URL}/landing.jpg`} alt="Landing Page" />
-                </Box>
+                    <Button colorScheme="blue" size="lg">
+                        Browse
+                    </Button>
+                </VStack>
             </Flex>
         </>
     );
