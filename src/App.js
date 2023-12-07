2eb668288b9af30b92c7753e21a281d5c30330bb
Seleem <sseleem1601@gmail.com>
2023-12-07 17:04:04 -0500

    Link landingpage.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';  // Your HomePage component
 import './App.css';
 import WatchlistPage from './pages/WatchlistPage'; // Your WatchlistPage component
+import LandingPage from './pages/LandingPage'; // Your LandingPage component
 
 // Import other pages and components as needed
 
---
+++ src/pages/LandingPage.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Box, Text, Button, Image, Flex, Spacer } from '@chakra-ui/react';
+import Header from '../components/Header'; // Assuming you have a Header component
+
+const LandingPage = () => {
+    return (
+        <>
+            <Header />
+            <Flex direction="column" align="center" justify="center" minHeight="100vh" bg="white" px={4}>
+                <Box maxW={{ xl: "1200px" }} width="full">
+                    <Text fontSize="5xl" fontWeight="bold" textAlign="center" mb={6}>
+                        Welcome to Our World of Books!
+                    </Text>
+                    <Text fontSize="xl" textAlign="center" mb={8}>
+                        Discover your next great read with us. Explore new releases, trending titles, and timeless classics.
+                    </Text>
+                    <Flex justify="center">
+                        <Button colorScheme="blue" mr={3}>
+                            Browse Books
+                        </Button>
+                        <Button colorScheme="green">
+                            Join our Community
+                        </Button>
+                    </Flex>
+                </Box>
+                <Spacer />
+                <Box maxW={{ xl: "1200px" }} width="full">
+                    <Image src={`${process.env.PUBLIC_URL}/landing-page-image.jpg`} alt="Landing Page" />
+                </Box>
+            </Flex>
+        </>
+    );
+};
+
+export default LandingPage;
