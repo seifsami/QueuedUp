77d8c104945d0b5331e445be1099642961feb5f5
Seleem <sseleem1601@gmail.com>
2023-12-07 18:47:39 -0500

    Seperate js and css

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Text, Button, Flex, VStack } from '@chakra-ui/react';
 import { motion } from 'framer-motion';
+import './LandingPage.css'; // Import the CSS file
 
-// Create a motion component
 const MotionButton = motion(Button);
 
 const LandingPage = () => {
@@ -14,50 +14,24 @@ const LandingPage = () => {
 
     return (
         <>
-            <Flex
-                direction="column"
-                align="center"
-                justify="center"
-                minHeight="100vh"
-                bgImage={`${process.env.PUBLIC_URL}/landing.jpg`} // Background image
-                bgPosition="center"
-                bgRepeat="no-repeat"
-                bgSize="cover"
-            >
-                <VStack spacing={6} maxW={{ xl: "1200px" }} width="full" px={4} p={6}>
-                    <Text 
-                        fontSize="6xl" 
-                        fontWeight="extrabold" 
-                        textAlign="center" 
-                        color="white"
-                        textShadow="0 0 10px black, 0 0 12px black, 0 0 14px black" // Enhanced first shadow
-                    >
+            <Flex className="landing-page-flex" bgImage={`${process.env.PUBLIC_URL}/landing.jpg`}>
+                <VStack className="landing-page-vstack">
+                    <Text className="title-text">
                         QueuedUp
                     </Text>
-                    <Text 
-                        fontSize="2xl" 
-                        fontWeight="bold" 
-                        textAlign="center" 
-                        color="white"
-                        textShadow="0 0 10px black, 0 0 12px black, 0 0 14px black" // Same shadow effect for consistency
-                    >
+                    <Text className="subtitle-text">
                         Get notified when your favorite books, movies, and TV shows are available.
                     </Text>
                     <MotionButton
-                        bg="black" // Set background color to black
-                        color="white" // Set text color to white for contrast
+                        className="motion-button"
                         size="lg"
                         initial="hidden"
                         animate="visible"
                         variants={variants}
                         transition={{ duration: 0.2, delay: 0.3 }}
-                        boxShadow="0 4px 12px 0 rgba(0,0,0,0.5)" // Shadow for depth
-                        borderRadius="md" // Slightly rounded corners
-                        whileHover={{ scale: 1.05, backgroundColor: "gray.700" }} // Slightly change color on hover
-                        whileFocus={{ scale: 1.05, backgroundColor: "gray.700" }} // Slightly change color on focus
-                        whileTap={{ scale: 0.95 }} // Scale down slightly on tap/click
-                        _hover={{ textDecoration: 'none' }} // Remove text underline on hover
-                        _focus={{ outline: 'none' }} // Remove focus outline
+                        whileHover={{ scale: 1.05 }}
+                        whileFocus={{ scale: 1.05 }}
+                        whileTap={{ scale: 0.95 }}
                     >
                         Browse
                     </MotionButton>
