9b104eb4c1e1aca78c0475ad5bc871bfb6d9eebe
Seleem <sseleem1601@gmail.com>
2023-12-14 11:41:16 -0500

    Styling detailsmodal

--- src/components/DetailsModal.js
+++ src/components/DetailsModal.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, Button, Image, Text, Flex } from '@chakra-ui/react';
+import {
+  Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody,
+  ModalCloseButton, Button, Image, Text, Flex, Box
+} from '@chakra-ui/react';
 
 const DetailsModal = ({ isOpen, onClose, item }) => {
   if (!item) {
@@ -7,27 +10,40 @@ const DetailsModal = ({ isOpen, onClose, item }) => {
   }
 
   return (
-    <Modal isOpen={isOpen} onClose={onClose} size={{ base: "md", md: "lg" }}>
+    <Modal isOpen={isOpen} onClose={onClose} size={{ base: "sm", md: "lg" }} isCentered>
       <ModalOverlay />
-      <ModalContent>
+      <ModalContent 
+        maxWidth={{ base: "96%", md: "70%" }} // Slightly less than full width on mobile for margin
+        overflowY="auto"
+        m={{ base: 2, md: 0 }} // Margin on mobile
+      >
         <ModalHeader textAlign="center">{item.title}</ModalHeader>
         <ModalCloseButton />
         <ModalBody>
-          <Flex flexDirection="column" alignItems="center" justifyContent="center">
-            <Image
-              src={item.image}
-              alt={item.title}
-              width="50vh"
-              height="50vh"
-              marginBottom="20px"
-              objectFit="contain"
-            />
-            <Text fontSize="lg">Release Date: {new Date(item.releaseDate).toLocaleDateString()}</Text>
+          <Flex 
+            direction={{ base: "column", md: "row" }} 
+            alignItems="center" 
+            justifyContent="center"
+          >
+            <Box flexShrink={0} mb={{ base: 4, md: 0 }}>
+              <Image
+                src={item.image}
+                alt={item.title}
+                width={{ base: "30vh", md: "50vh" }}
+                height={{ base: "30vh", md: "50vh" }}
+                objectFit="contain"
+              />
+            </Box>
+            <Box flex="1" px={{ base: 2, md: 4 }}>
+              <Text fontSize="lg" fontWeight="bold" mb={2}>
+                Release Date: {new Date(item.releaseDate).toLocaleDateString()}
+              </Text>
+              {/* Add more details as needed */}
+            </Box>
           </Flex>
-          {/* Add more details as needed */}
         </ModalBody>
         <ModalFooter justifyContent="center">
-          <Button colorScheme="teal" size="sm" onClick={onClose}>
+          <Button colorScheme="teal" size="md" onClick={onClose}>
             Close
           </Button>
         </ModalFooter>
