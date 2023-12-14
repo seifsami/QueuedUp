c56a994ef89a39132e9ae6b884426d4005649117
Seleem <sseleem1601@gmail.com>
2023-12-14 17:08:45 -0500

    Add notify me button

--- src/components/DetailsModal.js
+++ src/components/DetailsModal.js
@@ -9,6 +9,11 @@ const DetailsModal = ({ isOpen, onClose, item }) => {
     return null; // Don't render the modal if item is null
   }
 
+  const handleNotifyMe = () => {
+    // Implement the logic for Notify Me action
+    console.log('Notify Me clicked for item:', item.title);
+  };
+
   return (
     <Modal isOpen={isOpen} onClose={onClose} size={{ base: "sm", md: "lg" }} isCentered>
       <ModalOverlay />
@@ -30,12 +35,12 @@ const DetailsModal = ({ isOpen, onClose, item }) => {
                 alt={item.title}
                 width={{ base: "30vh", md: "50vh" }}
                 height={{ base: "30vh", md: "50vh" }}
-                mt = {4}
+                mt = {6}
                 objectFit="contain"
               />
             </Box>
             <Box flex="1" px={{ base: 2, md: 4 }}>
-              <Text fontSize="2xl" fontWeight="bold" mt = {{base:"0", md:"15px"}} mb={2} textAlign={{ base: "center", md: "left" }}>
+              <Text fontSize="2xl" fontWeight="bold" mt = {{base:"0", md:"30px"}} mb={2} textAlign={{ base: "center", md: "left" }}>
                 {item.title}
               </Text>
               {item.author && (
@@ -55,6 +60,9 @@ const DetailsModal = ({ isOpen, onClose, item }) => {
           </Flex>
         </ModalBody>
         <ModalFooter justifyContent="center">
+          <Button colorScheme="green" size="lg" mr={3} onClick={handleNotifyMe}>
+            Notify Me!
+          </Button>
           <Button colorScheme="teal" size="md" onClick={onClose}>
             Close
           </Button>
