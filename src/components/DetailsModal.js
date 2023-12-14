1edfaccf5a6cd5f4e023a941074a92e6186e2e76
Seleem <sseleem1601@gmail.com>
2023-12-14 11:55:34 -0500

    Centered image

--- src/components/DetailsModal.js
+++ src/components/DetailsModal.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody,
+  Modal, ModalOverlay, ModalContent, ModalFooter, ModalBody,
   ModalCloseButton, Button, Image, Text, Flex, Box
 } from '@chakra-ui/react';
 
@@ -13,32 +13,44 @@ const DetailsModal = ({ isOpen, onClose, item }) => {
     <Modal isOpen={isOpen} onClose={onClose} size={{ base: "sm", md: "lg" }} isCentered>
       <ModalOverlay />
       <ModalContent 
-        maxWidth={{ base: "96%", md: "70%" }} // Slightly less than full width on mobile for margin
+        maxWidth={{ base: "96%", md: "70%" }}
         overflowY="auto"
-        m={{ base: 2, md: 0 }} // Margin on mobile
+        m={{ base: 2, md: 0 }}
       >
-        <ModalHeader textAlign="center">{item.title}</ModalHeader>
         <ModalCloseButton />
         <ModalBody>
           <Flex 
             direction={{ base: "column", md: "row" }} 
-            alignItems="center" 
+            alignItems={{ base: "center", md: "start" }} 
             justifyContent="center"
           >
-            <Box flexShrink={0} mb={{ base: 4, md: 0 }}>
+            <Box flexShrink={0} mb={{ base: 4, md: 0 }} textAlign={{ base: "center", md: "left" }}>
               <Image
                 src={item.image}
                 alt={item.title}
                 width={{ base: "30vh", md: "50vh" }}
                 height={{ base: "30vh", md: "50vh" }}
+                mt = {4}
                 objectFit="contain"
               />
             </Box>
             <Box flex="1" px={{ base: 2, md: 4 }}>
-              <Text fontSize="lg" fontWeight="bold" mb={2}>
+              <Text fontSize="lg" fontWeight="bold" mt = {{base:"0", md:"15px"}} mb={2} textAlign={{ base: "center", md: "left" }}>
+                {item.title}
+              </Text>
+              {item.author && (
+                <Text fontSize="md" mb={2} textAlign={{ base: "center", md: "left" }}>
+                  Author: {item.author}
+                </Text>
+              )}
+              <Text fontSize="md" mb={2} textAlign={{ base: "center", md: "left" }}>
                 Release Date: {new Date(item.releaseDate).toLocaleDateString()}
               </Text>
-              {/* Add more details as needed */}
+              {item.description && (
+                <Text fontSize="md" textAlign={{ base: "center", md: "left" }}>
+                  Description: {item.description}
+                </Text>
+              )}
             </Box>
           </Flex>
         </ModalBody>
