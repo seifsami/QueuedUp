ea53199309e9b6d1d268751dc9fb28a11cb7c82a
Seleem <sseleem1601@gmail.com>
2023-12-14 11:29:19 -0500

    Adjust styling

--- src/components/DetailsModal.js
+++ src/components/DetailsModal.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, Button, Image, Text } from '@chakra-ui/react';
+import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, Button, Image, Text, Flex } from '@chakra-ui/react';
 
 const DetailsModal = ({ isOpen, onClose, item }) => {
   if (!item) {
@@ -10,22 +10,24 @@ const DetailsModal = ({ isOpen, onClose, item }) => {
     <Modal isOpen={isOpen} onClose={onClose} size={{ base: "md", md: "lg" }}>
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader>{item.title}</ModalHeader>
+        <ModalHeader textAlign="center">{item.title}</ModalHeader>
         <ModalCloseButton />
         <ModalBody>
-          <Image
-            src={item.image}
-            alt={item.title}
-            width="50vh"
-            height="50vh"
-            marginBottom="20px"
-            objectFit="contain"
-          />
-          <Text fontSize="lg">Release Date: {new Date(item.releaseDate).toLocaleDateString()}</Text>
+          <Flex flexDirection="column" alignItems="center" justifyContent="center">
+            <Image
+              src={item.image}
+              alt={item.title}
+              width="50vh"
+              height="50vh"
+              marginBottom="20px"
+              objectFit="contain"
+            />
+            <Text fontSize="lg">Release Date: {new Date(item.releaseDate).toLocaleDateString()}</Text>
+          </Flex>
           {/* Add more details as needed */}
         </ModalBody>
-        <ModalFooter>
-            <Button  colorScheme="teal" size="sm" flex={2} onClick={onClose}>
+        <ModalFooter justifyContent="center">
+          <Button colorScheme="teal" size="sm" onClick={onClose}>
             Close
           </Button>
         </ModalFooter>
