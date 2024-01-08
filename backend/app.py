0b7b927bb0b405b36f0f8c1fc381fdfd05926994
Seleem <sseleem1601@gmail.com>
2024-01-08 09:42:54 -0500

    Add search endpoint

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
@@ -12,10 +12,48 @@ def hello():
 
 @app.route('/test_mongodb')
 def test_mongodb():
-    # Test MongoDB connection
-    # For example, list the names of the databases
-    db_list = mongo.cx.list_database_names()
-    return jsonify(db_list)
+    try:
+        db_list = mongo.cx.list_database_names()
+        return jsonify(db_list)
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+@app.route('/search')
+def search():
+    query = request.args.get('query')  # or request.json['query']
+
+    try:
+        # Adjust with your actual database name
+        db = mongo.cx.queuedupdb
+
+        # Search in movies collection
+        movies_results = db.movies.aggregate([
+            {
+                "$search": {
+                    "text": {
+                        "query": query,
+                        "path": ["title", "description", "original_title", "director"]  # adjust fields as necessary
+                    }
+                }
+            }
+        ])
+
+        # Search in tv_seasons collection
+        tv_results = db.tv_seasons.aggregate([
+            {
+                "$search": {
+                    "text": {
+                        "query": query,
+                        "path": ["title", "description", "original_name", "network_name"]  # adjust fields as necessary
+                    }
+                }
+            }
+        ])
+
+        combined_results = list(movies_results) + list(tv_results)
+        return jsonify(combined_results)
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
 if __name__ == '__main__':
     app.run(debug=True)
