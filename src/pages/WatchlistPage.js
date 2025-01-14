b576f66434d1e0b05699f741755ec0213801555a
seifsami <seifwsami@gmail.com>
2025-01-13 23:39:10 -0500

    fixed watchlist page

--- src/pages/WatchlistPage.js
+++ src/pages/WatchlistPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Heading,
   Select,
@@ -11,119 +11,133 @@ import {
   Tabs,
   TabList,
   Tab,
+  Spinner,
+  Center,
 } from '@chakra-ui/react';
 import Header from '../components/Header';
 import WatchlistPreviewCard from '../components/WatchlistPreviewCard';
+import { getUserWatchlist } from '../services/api';
 
-// Dummy data
-const initialWatchlistData = [
-    { id: 1, title: "Stranger Things Season 2", series: "Stranger Things", type:"tv", image:`${process.env.PUBLIC_URL}51J4VWwlmvL.jpg`,creator: "Matt Dinniman", releaseDate: '2023-12-30', dateAdded:'2023-12-01'},
-    { id: 2, title: "American Dad Season 12", series: "American Dad",  type: "tv", image:`${process.env.PUBLIC_URL}Americandad.jpeg`, creator: "Seleem Seleem ", releaseDate: '2024-09-30', dateAdded:'2023-12-02'},
-    { id: 3, title: "Family Switch", series: "N/A" ,  type: "movie", image:`${process.env.PUBLIC_URL}familyswitch.jpeg`, creator: "Steven Spielberg", releaseDate: '2024-03-30', dateAdded:'2023-12-03'},
-    { id: 4, title: "Malcolm In The Middle Season 6", series: "Malcolm In The Middle",  type: "tv", image:`${process.env.PUBLIC_URL}Malcolm.jpeg`, creator: "Steven Spielberg", releaseDate: '2023-11-30', dateAdded:'2023-12-04'},
-    { id: 5, title: "Oppenheimer", series: "N/A",  type: "movie", image:`${process.env.PUBLIC_URL}oppenheimer.jpeg`, creator: "Christopher Nolan", releaseDate: '2024-12-30', dateAdded:'2023-12-05'},
-    { id: 6, title: "The Simpsons Season 30", series: "The Simpsons",  type: "tv", image:`${process.env.PUBLIC_URL}simpsons.jpeg`, creator: "Matt Groening", releaseDate: '2025-12-30', dateAdded:'2023-12-06'},
-    { id: 7, title: "What if?", series: "Marvel Mysteries",  type: "tv", image:`${process.env.PUBLIC_URL}whatif.jpeg`, creator: "Steven Spielberg", releaseDate: '2024-12-20', dateAdded:'2023-12-07'},
-    { id: 8, title: "Tomorrow, and Tomorrow, And Tomorrow", series: "N/A",  type: "book", image:`${process.env.PUBLIC_URL}tomorrow.jpeg`, creator: "Gabrielle Zevin", releaseDate: '2024-12-21', dateAdded:'2023-12-08'},
-    { id: 9, title: "The Name Of The Wind", series: "The KingKiller Chronicles",  type: "book", image:`${process.env.PUBLIC_URL}rothfuss.webp`, creator: "Patrick Rothfuss", releaseDate: '2024-12-22', dateAdded:'2023-12-09'},
-    { id: 10, title: "Master and Margarita", series: "N/A",  type: "book", image:`${process.env.PUBLIC_URL}master.jpeg`, creator: "Mikhail Bulgagov", releaseDate: '2024-12-23', dateAdded:'2023-12-10'}
+const filterOptions = [
+  { value: 'all', label: 'All' },
+  { value: 'books', label: 'Books' },
+  { value: 'tv_seasons', label: 'TV Shows' },
+  { value: 'movies', label: 'Movies' },
 ];
-
-
-const filterOptions = ['all', 'book', 'tv', 'movie'];
 const sortOptions = {
   releaseDate: 'Release Date',
   title: 'Title',
   dateAdded: 'Date Added',
 };
 
-  
-const WatchlistPage = () => { 
-    const [watchlistData] = useState(initialWatchlistData); //Replace with: const [watchlistData, setWatchlistData] = useState(initialWatchlistData);
-    const [tabIndex, setTabIndex] = useState(0);
-    const [sortCriterion, setSortCriterion] = useState('dateAdded');
-    const [releaseStatus, setReleaseStatus] = useState('upcoming');
+const WatchlistPage = ({ user }) => {
+  const [watchlistData, setWatchlistData] = useState([]);
+  const [tabIndex, setTabIndex] = useState(0);
+  const [sortCriterion, setSortCriterion] = useState('dateAdded');
+  const [releaseStatus, setReleaseStatus] = useState('upcoming');
+  const [loading, setLoading] = useState(false);
 
-    const handleReleaseStatusChange = (status) => {
-        setReleaseStatus(status);
-    };
-    
-    const handleSortChange = (e) => {
-        setSortCriterion(e.target.value);
-    };
+  // Fetch the user's watchlist from the backend
+  const fetchUserWatchlist = async () => {
+    if (!user) return;
+    try {
+      setLoading(true);
+      const { data } = await getUserWatchlist(user.uid);
+      console.log('Fetched watchlist:', data);
+      setWatchlistData(data); // Update state with fetched data
+      setLoading(false);
+    } catch (err) {
+      console.error('Failed to fetch watchlist:', err);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchUserWatchlist();
+  }, [user]);
+
+  const handleReleaseStatusChange = (status) => {
+    setReleaseStatus(status);
+  };
+
+  const handleSortChange = (e) => {
+    setSortCriterion(e.target.value);
+  };
 
   // Filter and sort logic
   const filteredData = watchlistData
-  .filter((item) => {
-    // Filter based on release status
-    const today = new Date();
-    const releaseDate = new Date(item.releaseDate);
-    const isUpcoming = releaseDate >= today;
-    return releaseStatus === 'upcoming' ? isUpcoming : !isUpcoming;
-  })
-  .filter((item) => {
-    // Filter based on selected tab, 'all' shows all types
-    const selectedType = filterOptions[tabIndex];
-    return selectedType === 'all' || item.type === selectedType;
-  })
+    .filter((item) => {
+      // Filter based on release status
+      const today = new Date();
+      const releaseDate = new Date(item.release_date);
+      const isUpcoming = releaseDate >= today;
+      return releaseStatus === 'upcoming' ? isUpcoming : !isUpcoming;
+    })
+    .filter((item) => {
+      // Filter based on selected tab, 'all' shows all types
+      const selectedType = filterOptions[tabIndex].value;
+      return selectedType === 'all' || item.media_type === selectedType;
+
+    })
     .sort((a, b) => {
       if (sortCriterion === 'releaseDate') {
-        return new Date(a.releaseDate) - new Date(b.releaseDate);
+        return new Date(a.release_date) - new Date(b.release_date);
       } else if (sortCriterion === 'title') {
         return a.title.localeCompare(b.title);
       } else if (sortCriterion === 'dateAdded') {
-        return new Date(a.dateAdded) - new Date(b.dateAdded);
+        return new Date(a.timestamp_added) - new Date(b.timestamp_added);
       }
       return 0;
     });
-    
-  
-    return (
-        <>
-          <Header />
-          <Container maxW="1200px" mx="auto" p={4} bg="white">
-            <Heading as="h2" size="xl" mb={4}>My Tracking List</Heading>
-            <Tabs index={tabIndex} onChange={setTabIndex} variant="soft-rounded" colorScheme="green" pb={2}>
-                <TabList>
-                {filterOptions.map((filterOption, index) => (
-                    <Tab key={filterOption} isSelected={tabIndex === index}>
-                    {filterOption.charAt(0).toUpperCase() + filterOption.slice(1)}
-                    </Tab>
-                ))}
-                </TabList>
-            </Tabs>
-            <HStack spacing={4} mb={4}>
-              <ButtonGroup isAttached variant="outline">
-                <Button 
-                  isActive={releaseStatus === 'upcoming'} 
-                  onClick={() => handleReleaseStatusChange('upcoming')}
-                >
-                  Upcoming
-                </Button>
-                <Button 
-                  isActive={releaseStatus === 'released'} 
-                  onClick={() => handleReleaseStatusChange('released')}
-                >
-                  Released
-                </Button>
-              </ButtonGroup>
-              <Select onChange={handleSortChange} value={sortCriterion} size="sm">
-                {Object.entries(sortOptions).map(([value, label]) => (
-                  <option key={value} value={value}>{label}</option>
-                ))}
-              </Select>
-            </HStack>
-            <VStack spacing={4} align="stretch">
-              {filteredData.map((item) => (
-                <WatchlistPreviewCard key={item.id} item={item} />
-              ))}
-              {filteredData.length === 0 && (
-                <Text py={10} textAlign="center">Your watchlist is currently empty.</Text>
-              )}
-            </VStack>
-          </Container>
-        </>
-      );
-    };
-    
-    export default WatchlistPage;
+
+  return (
+    <>
+      <Header />
+      <Container maxW="1200px" mx="auto" p={4} bg="white">
+        <Heading as="h2" size="xl" mb={4}>
+          My Tracking List
+        </Heading>
+        <Tabs index={tabIndex} onChange={setTabIndex} variant="soft-rounded" colorScheme="green" pb={2}>
+          <TabList>
+            {filterOptions.map((filterOption, index) => (
+              <Tab key={filterOption.value} isSelected={tabIndex === index}>
+                {filterOption.label}
+              </Tab>
+            ))}
+          </TabList>
+        </Tabs>
+        <HStack spacing={4} mb={4}>
+          <ButtonGroup isAttached variant="outline">
+            <Button isActive={releaseStatus === 'upcoming'} onClick={() => handleReleaseStatusChange('upcoming')}>
+              Upcoming
+            </Button>
+            <Button isActive={releaseStatus === 'released'} onClick={() => handleReleaseStatusChange('released')}>
+              Released
+            </Button>
+          </ButtonGroup>
+          <Select onChange={handleSortChange} value={sortCriterion} size="sm">
+            {Object.entries(sortOptions).map(([value, label]) => (
+              <option key={value} value={value}>
+                {label}
+              </option>
+            ))}
+          </Select>
+        </HStack>
+        {loading ? (
+          <Center>
+            <Spinner size="xl" />
+          </Center>
+        ) : (
+          <VStack spacing={4} align="stretch">
+            {filteredData.map((item) => (
+              <WatchlistPreviewCard key={item._id} item={item} />
+            ))}
+            {filteredData.length === 0 && <Text py={10} textAlign="center">Your watchlist is currently empty.</Text>}
+          </VStack>
+        )}
+      </Container>
+    </>
+  );
+};
+
+export default WatchlistPage;
