6e4715a806710078b47810addf60b39d9a86e059
seifsami <seifwsami@gmail.com>
2025-01-29 18:48:57 -0500

    Debugging MongoDB connection issue

--- backend/app.py
+++ backend/app.py
@@ -4,8 +4,9 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
-app.config["MONGO_URI"] = "mongodb+srv://seifsami:beesknees@queuedupdbnew.lsrfn46.mongodb.net/?retryWrites=true&w=majority"
-mongo = PyMongo(app)
+app.config["MONGO_URI"] = os.getenv("MONGO_URI")mongo = PyMongo(app)
+if not MONGO_URI:
+    print("🚨 ERROR: MONGO_URI is NOT set in environment variables!")
 app.extensions['pymongo'] = mongo  # Ensure the mongo object is available in the app context
 
 from blueprints.user import user_blueprint
